961fbff4653824eb84455dc33cf611b116535272
Masami Yonehara <zeitdiebe@gmail.com>
2019-07-04 20:18:09 +0900

    [from now] 2019/07/04 20:18:09
    diff --git a/26. Remove Duplicates from Sorted Array/using pointer.js b/26. Remove Duplicates from Sorted Array/using pointer.js
    index f0061c6..910cd76 100644
    --- a/26. Remove Duplicates from Sorted Array/using pointer.js
    +++ b/26. Remove Duplicates from Sorted Array/using pointer.js
    @@ -3,15 +3,23 @@
      * @return {number}
      */
     var removeDuplicates = function(nums) {
    -  const l = nums.length
       let pointer1 = 0
       let pointer2 = 0
    
    -  while(pointer2 >= l) {
    -    if (nums[pointer1] === nums[pointer2]) {
    -      pointer2 += 1
    -    } else {
    -      break;
    +  while(pointer1 < nums.length) {
    +    while(pointer2 <= nums.length) {
    +      if (nums[pointer1] === nums[pointer2]) {
    +        pointer2 += 1
    +      } else {
    +        nums[pointer1] = nums[pointer2 - 1]
    +        break;
    +      }
         }
       }
    +
    +  return nums.length
     }
    +
    +
    +console.log(removeDuplicates([1,1,2]))
    +console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))

--- 26. Remove Duplicates from Sorted Array/using pointer.js
+++ 26. Remove Duplicates from Sorted Array/using pointer.js
@@ -3,15 +3,23 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  const l = nums.length
   let pointer1 = 0
   let pointer2 = 0
 
-  while(pointer2 >= l) {
-    if (nums[pointer1] === nums[pointer2]) {
-      pointer2 += 1
-    } else {
-      break;
+  while(pointer1 < nums.length) {
+    while(pointer2 <= nums.length) {
+      if (nums[pointer1] === nums[pointer2]) {
+        pointer2 += 1
+      } else {
+        nums[pointer1] = nums[pointer2 - 1]
+        break;
+      }
     }
   }
+
+  return nums.length
 }
+
+
+console.log(removeDuplicates([1,1,2]))
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
